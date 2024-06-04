10ff85468c994086c873219382cfda2a59194cb8
Derek Alba <patoalba7@gmail.com>
2024-06-04 17:04:49 -0600

    proyecto terminado

--- assets/main.js
+++ assets/main.js
@@ -1,83 +1,142 @@
 let botones = document.querySelectorAll('.btns')
 let accion = document.querySelectorAll('.op')
 let pantalla = document.querySelector('#consola')
-let numeroA;
-let numeroB;
-let acumulador;
-let signo;
-let acumuladorMulti = 1
-let acumuladorDivi;
-let acumuladoA;
+let limpieza = document.querySelector('#limpiar')
+let numeroA = 0
+let numeroB = 0
+let acumulador = 0
+let signo = ''
+let primeraVez = true
 
 botones.forEach((btn) => {
   btn.addEventListener('click', () => {
     pantalla.value += btn.value
 
   })
-
 })
 
+const suma = () => {
+  acumulador = acumulador + numeroA
+}
 
+const resta = () => {
+  if (primeraVez) {
+    acumulador = numeroA - Math.abs(acumulador)
+    primeraVez = false
+  }
+  else if (acumulador >= 0) {
+    acumulador = Math.abs(acumulador) - numeroA
+  }
+  else {
+    acumulador = acumulador - numeroA
+  }
+}
+
+const multiplicacion = () => {
+  if (acumulador == 0) {
+    acumulador = 1;
+    acumulador = acumulador * numeroA
+  }
+  else {
+    acumulador = acumulador * numeroA
+  }
+}
+
+const division = () => {
+  if (acumulador == 0) {
+    acumulador = numeroA
+  }
+  else {
+    acumulador = acumulador / numeroA
+  }
+}
+
+const igual = () => {
+  if (signo == '+') {
+    numeroB = parseInt(pantalla.value)
+    acumulador = acumulador + numeroB
+    pantalla.value = acumulador
+  }
+  else if (signo == '-') {
+    numeroB = parseInt(pantalla.value)
+    acumulador = acumulador - numeroB
+    primeraVez = true
+    pantalla.value = acumulador
+  }
+  else if (signo == '*') {
+    numeroB = parseInt(pantalla.value)
+    acumulador = acumulador * numeroB
+    pantalla.value = acumulador
+
+  } 
+  else if (signo == '/') 
+  {
+    numeroB = parseInt(pantalla.value)
+    acumulador = acumulador / numeroB
+    pantalla.value = acumulador
+  }
+}
 accion.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    if (e.target.value == '+' || e.target.value == '-' || e.target.value == '/' || e.target.value == '*') {
+    if ( pantalla.value == "") {
+      alert("INGRESE UN NUMERO")
+    }
+    else if (e.target.value != '=') {
       numeroA = parseInt(pantalla.value)
       switch (e.target.value) {
         case '+':
-          acumulador = acumulador + numeroA
+          if (signo != '') {
+            igual();
+          }
+          else {
+            suma()
+          }
           break;
         case '-':
-          acumulador = numeroA - acumulador
+          if (signo != '') {
+            igual();
+          }
+          else {
+            resta()
+          }
           break;
         case '*':
-          acumuladorMulti = acumuladorMulti * numeroA
+          if (signo != '') {
+            igual();
+          }
+          else {
+            multiplicacion()
+          }
           break;
         case '/':
-          if (acumuladorDivi == 0) {
-            acumuladoA = parseInt(numeroA)
-            acumuladorDivi++
+          if (signo != '') {
+            igual();
           }
-          else if (acumuladorDivi > 0) {
-            acumuladoA = acumuladoA / numeroA
-            numeroA = acumuladoA
+          else {
+            division()
           }
-
-
           break;
+
       }
       signo = e.target.value
       pantalla.value = ''
     }
 
-    else if (e.target.value == '=') {
-      if (signo == '+') {
-        numeroB = parseInt(pantalla.value)
-        let resultado = acumulador + numeroB
-        pantalla.value = resultado
-      } else if (signo == '-') {
-        numeroB = parseInt(pantalla.value)
-        let resultado = acumulador - numeroB
-        pantalla.value = resultado
-      } else if (signo == '*') {
-        numeroB = parseInt(pantalla.value)
-        let resultado = acumuladorMulti * numeroB
-        pantalla.value = resultado
-      } else if (signo == '/') {
-        numeroB = parseInt(pantalla.value)
-        let resultado = numeroA / numeroB
-        pantalla.value = resultado
-      }
+    else {
+      igual()
       acumulador = 0;
-      acumuladorMulti = 1;
+      signo = ''
     }
-    else if (e.target.value == 'C') {
-      pantalla.value = ''
-
-    }
-
   })
+})
 
-
+limpieza.addEventListener('click', () => {
+  numeroA = 0
+  numeroB = 0
+  acumulador = 0
+  signo = ''
+  primeraVez = true
+  pantalla.value = ''
 })
 
 
